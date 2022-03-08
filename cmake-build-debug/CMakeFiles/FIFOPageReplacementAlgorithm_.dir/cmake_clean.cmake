88f6b4b1ef667c542f76faa4ce41fb7e762795cf
Majid Meo <60882485+Majid786125@users.noreply.github.com>
2022-03-09 00:33:47 +0500

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/FIFOPageReplacementAlgorithm_.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FIFOPageReplacementAlgorithm_.dir/main.cpp.obj"
+  "FIFOPageReplacementAlgorithm_.exe"
+  "FIFOPageReplacementAlgorithm_.exe.manifest"
+  "FIFOPageReplacementAlgorithm_.pdb"
+  "libFIFOPageReplacementAlgorithm_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FIFOPageReplacementAlgorithm_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
